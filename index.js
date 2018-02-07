b353317f9734cf0da308d882014263af2d294608
FTeddy <ainsophoure@gmail.com>
2018-02-07 16:37:48 +0700

    removed unused lines

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function ticTacBoard(){
   let board = { row: 3, collumn: 3 };
-  let boardVol = board.row * board.collumn;
   let rows = [];
   let xo = 'XO';
   let XCount = 0;
@@ -11,10 +10,8 @@ function ticTacBoard(){
   for (let i = 0; i < rows.length; i++) {
     for (let j = 1; j <= board.collumn; j++) {
       if (XCount < 5 && OCount < 5) {
-        // inserting. call x or o
         let input = genXO();
         rows[i].push(input);
-        // if input is x
         if (input === 'X') {
           XCount += 1;
         } else {
@@ -38,7 +35,6 @@ function ticTacBoard(){
   return rows;
 }
 
-// generate x or o
 function genXO(){
   let xo = 'XO';
   let randomIO = Math.round(Math.random());
