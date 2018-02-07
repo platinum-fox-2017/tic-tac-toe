33bc31c4377f66715f95acd813c01d2ddf8a89cb
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-07 16:54:14 +0700

    Udah felksibel

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-function ticTacToe() {
-    var tic_tac_toe=[[],[],[]];
-    var availableIndex=[0,1,2,3,4,5,6,7,8];
-    for(var i=0; i<9; i++) {
-        var index=Math.floor(Math.random()*(availableIndex.length));
-        var row=availableIndex[index]%3;
-        var col=Math.floor(availableIndex[index]/3);
+function ticTacToe(num) {
+    var tic_tac_toe=[];
+    var usedIndex=[];
+    for(var x=0; x<num; x++) tic_tac_toe.push([]);
+    console.log(tic_tac_toe)
+    for(var i=0; i<Math.pow(num,2); i++) {
+        var index=Math.floor(Math.random()*(Math.pow(num,2)));
+        if(usedIndex.indexOf(index)!==-1) {
+            while(usedIndex.indexOf(index)!==-1){
+                index=Math.floor(Math.random()*(Math.pow(num,2)));
+            }
+        }
+        usedIndex.push(index);
+        var row=index%num;
+        var col=Math.floor(index/num);
         if(i%2===0) tic_tac_toe[col][row]='O';
         else tic_tac_toe[col][row]='X';
-        availableIndex.splice(index,1);
     }
     return tic_tac_toe;
 }
 
-console.log(ticTacToe());
+console.log(ticTacToe(5));
